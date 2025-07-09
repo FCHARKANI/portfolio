cba7bb65c69925afa966a960b8433e02065844a0
Fatima Ezzahra Charkani <charkani.fz@gmail.com>
2025-07-09 14:13:57 +0100

    Portfolio FZCharkani

---
+++ script.js
@@ -0,0 +1,2 @@
+var siteName = document.title;
+document.getElementById('copyright').innerText = `© ${new Date().getFullYear()} ${siteName}. Tous droits réservés.`;// Tu peux ajouter des animations ou interactions ici
