52bcb6939ad774613ab3895b2fda21918a7951b5
Fatima Ezzahra Charkani <charkani.fz@gmail.com>
2025-08-12 17:08:57 +0100

    change

--- script.js
+++
@@ -1,2 +0,0 @@
-var siteName = document.title;
-document.getElementById('copyright').innerText = `© ${new Date().getFullYear()} ${siteName}. Tous droits réservés.`;// Tu peux ajouter des animations ou interactions ici
